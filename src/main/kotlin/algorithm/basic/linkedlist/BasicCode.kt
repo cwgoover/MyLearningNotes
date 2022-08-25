7c910b03908b423375de179f1718e2ceb04e6530
Erica <erica.cao@sap.com>
2022-08-25 23:05:57 +0800

    complete the LinkedList own implementation

--- src/main/kotlin/algorithm/basic/linkedlist/BasicCode.kt
+++ src/main/kotlin/algorithm/basic/linkedlist/BasicCode.kt
@@ -9,17 +9,21 @@ object BasicCode {
 }
 
 fun main() {
-    val node1 = Mode.Node(value = 1)
-    val node2 = Mode.Node(value = 2)
-    val node3 = Mode.Node(value = 3)
-    val node4 = Mode.Node(value = 4)
-    val node5 = Mode.Node(value = 5)
+    "creating and linking nodes".run {
+        val node1 = Mode.Node(value = 1)
+        val node2 = Mode.Node(value = 2)
+        val node3 = Mode.Node(value = 3)
+        val node4 = Mode.Node(value = 4)
+        val node5 = Mode.Node(value = 5)
     
-    node1.next = node2
-    node2.next = node3
-    node3.next = node4
-    node4.next = node5
+        node1.next = node2
+        node2.next = node3
+        node3.next = node4
+        node4.next = node5
     
-    // You’ve just created five nodes
-    println(node1)
+        // You’ve just created five nodes
+        println(node1)
+        // ---Example of creating and linking nodes---
+        // 1 -> 2 -> 3 -> 4 -> 5
+    }
 }
--- src/main/kotlin/algorithm/basic/linkedlist/Mode.kt
+++ src/main/kotlin/algorithm/basic/linkedlist/Mode.kt
@@ -1,5 +1,7 @@
 package algorithm.basic.linkedlist
 
+import java.lang.IndexOutOfBoundsException
+
 /**
  * Created by i352072(erica.cao@sap.com) on 07/13/2022
  */
@@ -17,11 +19,113 @@ object Mode {
         }
     }
     
-    class LinkedList<T: Any> {
+    /**
+     * Now that you’ve implemented Iterator, you can do some really cool things. For example,
+     * you can iterate your linked list with a regular Kotlin for loop
+     *
+     */
+    class LinkedList<T: Any>: MutableCollection<T>, MutableIterable<T> {
+        // remove Iterable because a Collection is an Iterable anyway
+//    class LinkedList<T: Any>: Iterable<T>, Collection<T> {
+        
         private var head: Node<T>? = null
         private var tail: Node<T>? = null
-        private var size: Int = 0
+        // To make things easier, first make reading the size available outside the class,
+        // but its setter remains private
+        override var size: Int = 0
+            private set
     
+        override fun containsAll(elements: Collection<T>): Boolean {
+            // this is an inefficient method, it’s O(n^2)
+            for (element in elements) {
+                if (!contains(element)) return false
+            }
+            return true
+        }
+    
+        override fun contains(element: T): Boolean {
+            // This method iterates through all elements of the list if needed,
+            // so it has a complexity of O(n)
+            for (item in this) {
+                if (item == element) return true
+            }
+            return false
+        }
+    
+        override fun isEmpty(): Boolean = size == 0
+    
+        override fun iterator(): MutableIterator<T> = LinkedListIterator(this)
+        
+        override fun clear() {
+            // Performing clear() on a linked list is as easy as dropping all its node references
+            // and resetting the size to 0
+            head = null
+            tail = null
+            size = 0
+        }
+    
+        override fun addAll(elements: Collection<T>): Boolean {
+            for (element in elements) {
+                append(element)
+            }
+            // Since the LinkedList doesn’t have a fixed size, add() and addAll() are always
+            // successful and need to return true.
+            return true
+        }
+    
+        override fun add(element: T): Boolean {
+            append(element)
+            return true
+        }
+        
+        // It should remove any elements in the list besides the ones passed in as the parameter
+        override fun retainAll(elements: Collection<T>): Boolean {
+            var result = false
+            val iterator = iterator()
+            while (iterator.hasNext()) {
+                val item = iterator.next()
+                if (!elements.contains(item)) {
+                    iterator.remove()
+                    result = true
+                }
+            }
+            return result
+        }
+    
+        override fun removeAll(elements: Collection<T>): Boolean {
+            var result = false
+            for (element in elements) {
+//                remove(element)
+//                result = true
+                result = remove(element) || result
+            }
+            // The return value of removeAll is true if any elements were removed
+            return result
+        }
+    
+        override fun remove(element: T): Boolean {
+            // Get an iterator that will help you iterate through the collection.
+            val iterator = iterator()
+            // Create a loop that checks if there are any elements left, and gets the next one
+            while (iterator.hasNext()) {
+                if (iterator.next() == element) {
+                    // Check if the current element is the one you’re looking for, and if it is, remove it.
+                    iterator.remove()
+                    // Return a boolean that signals if an element has been removed
+                    return true
+                }
+            }
+            return false
+        }
+    
+        override fun toString(): String {
+            return if (isEmpty()) {
+                "Empty list"
+            } else {
+                head.toString()
+            }
+        }
+        
         /** Adds a value at the front of the list. This is also known as head-first insertion. */
         fun push(value: T): LinkedList<T> {
 //            val node = Node(value = value)
@@ -141,10 +245,10 @@ object Mode {
             
             val result = head?.value
             head = head?.next
-            // If the list becomes empty, you set tail to null as well.
-            if (isEmpty()) tail = null
             
             size--
+            // If the list becomes empty, you set tail to null as well.
+            if (isEmpty()) tail = null
             return result
         }
         
@@ -186,6 +290,7 @@ object Mode {
                 // this work to the pop function.
                 return pop()
             }
+            
             while (currentNode?.next != tail) {
                 currentNode = currentNode?.next
             }
@@ -218,65 +323,179 @@ object Mode {
             return afterNode?.value
         }
         
-        fun isEmpty(): Boolean = size == 0
+        private class LinkedListIterator<T: Any>(private val list: LinkedList<T>): MutableIterator<T> {
+            // You’ll need to keep track of the position that the iterator has in the collection,
+            // so create an index property inside the class
+            private var index: Int = 0
     
-        override fun toString(): String {
-            return if (isEmpty()) {
-                "Empty list"
-            } else {
-                head.toString()
+            // The next() method reads the values of your nodes in order, and use another property
+            // to help you out with its implementation. You’ll want to keep track of the last node
+            private var lastNode: Node<T>? = null
+            
+            override fun hasNext(): Boolean = index < list.size
+    
+            override fun next(): T {
+//                if (!hasNext()) throw NoSuchElementException()
+//                return list.nodeAt(index++)?.value ?: throw NoSuchElementException()
+                
+                if (index >= list.size) throw IndexOutOfBoundsException()
+    
+                // If this is the first iteration, there is no lastNode set, so you take the first
+                // node of the list. After the first iteration, you can get the next property of
+                // the last node to find the next one
+                lastNode = if (index == 0) list.nodeAt(0) else lastNode?.next
+                // Since the lastNode property was updated, you need to update the index too
+                index++
+                return lastNode!!.value
             }
+    
+            /** remove current node */
+            override fun remove() {
+                // The simplest case is when you’re at the beginning of the list.
+                // Using pop() will do the trick.
+                if (index == 1) {
+                    list.pop()
+                } else {
+                    // If the iterator is somewhere inside the list, it needs to find the previous
+                    // node. That’s the only way to remove items from a linked list.
+                    
+                    // Note: 这里index表示的是当前读取的个数，即从head开始算起的话，index是1起；可以参考next()中
+                    // lastNode = if (index == 0) list.nodeAt(0); index++ => 当lastNode = head时，index = 1
+                    // => 这里如果要找前一个node的话，index相当于list里的size值，所以前一个需要减2
+                    val preNode = list.nodeAt(index - 2) ?: return
+                    // The iterator needs to step back so that next() returns the correct method
+                    // the next time. This means reassigning the lastNode and decreasing the index.
+                    list.removeAfter(preNode)
+                    lastNode = preNode
+                }
+                index--
+            }
+    
         }
     }
     
 }
 
 fun main() {
-    // fluent interface push
     val list = Mode.LinkedList<Int>()
-    list.push(3).push(2).push(1)
-    println("fluent push list: $list")
-    // ---Example of push---
-    // 1 -> 2 -> 3
     
-    list.append(4)
-    println("After append: $list")
-    // ---Example of append---
-    // 1 -> 2 -> 3 -> 4
-    
-    var middleNode = list.nodeAt(1)!!
-    for (i in 1..3) {
-        middleNode = list.insert(-1 * i, middleNode)
+    "fluent interface push".run {
+        list.push(3).push(2).push(1)
+        println("fluent push list: $list")
+        println()
+        // ---Example of push---
+        // 1 -> 2 -> 3
     }
-    println("After inserting: newNode = $middleNode")
-    println("After inserting: $list")
-    // ---Example of inserting at a particular index---
-    // After inserting: newNode = -3 -> 3 -> 4
-    // After inserting: 1 -> 2 -> -1 -> -2 -> -3 -> 3 -> 4
-    println()
     
-    val popedValue = list.pop()
-    println("After popping list: $list")
-    println("Popped value: $popedValue")
-    // After popping list: 2 -> -1 -> -2 -> -3 -> 3 -> 4
-    // Popped value: 1
+    "append".run {
+        list.append(4)
+        println("After append: $list")
+        println()
+        // ---Example of append---
+        // 1 -> 2 -> 3 -> 4
+    }
     
-    list.append(10)
-    println("After append: $list")
-    val removedValue = list.removeLast()
-    println("After remove last: $list")
-    println("Removed value: $removedValue")
-    // ---Example of removing the last node---
-    // After append: 2 -> -1 -> -2 -> -3 -> 3 -> 4 -> 10
-    // After remove last: 2 -> -1 -> -2 -> -3 -> 3 -> 4
-    // Removed value: 10
+    "inserting at a particular index".run {
+        var middleNode = list.nodeAt(1)!!
+        for (i in 1..3) {
+            middleNode = list.insert(-1 * i, middleNode)
+        }
+        println("After inserting: newNode = $middleNode")
+        println("After inserting: $list")
+        println()
+        // ---Example of inserting at a particular index---
+        // After inserting: newNode = -3 -> 3 -> 4
+        // After inserting: 1 -> 2 -> -1 -> -2 -> -3 -> 3 -> 4
+    }
     
-    val index = 1
-    val node = list.nodeAt(index - 1)!!
-    val removedValue2 = list.removeAfter(node)
-    println("After removing at index $index: $list")
-    println("Removed value: $removedValue2")
-    // ---Example of removing a node after a particular node---
-    // After removing at index 1: 2 -> -2 -> -3 -> 3 -> 4
-    // Removed value: -1
+    "pop".run {
+        val popedValue = list.pop()
+        println("After popping list: $list")
+        println("Popped value: $popedValue")
+        println()
+        // ---Example of pop---
+        // After popping list: 2 -> -1 -> -2 -> -3 -> 3 -> 4
+        // Popped value: 1
+    }
+    
+    "removing the last node".run {
+        list.append(10)
+        println("After append: $list")
+        val removedValue = list.removeLast()
+        println("After remove last: $list")
+        println("Removed value: $removedValue")
+        println()
+        // ---Example of removing the last node---
+        // After append: 2 -> -1 -> -2 -> -3 -> 3 -> 4 -> 10
+        // After remove last: 2 -> -1 -> -2 -> -3 -> 3 -> 4
+        // Removed value: 10
+    }
+    
+    "removing a node after a particular node".run {
+        val index = 1
+        val node = list.nodeAt(index - 1)!!
+        val removedValue2 = list.removeAfter(node)
+        println("After removing at index $index: $list")
+        println("Removed value: $removedValue2")
+        println()
+        // ---Example of removing a node after a particular node---
+        // After removing at index 1: 2 -> -2 -> -3 -> 3 -> 4
+        // Removed value: -1
+    }
+    
+    "printing doubles".run {
+        // LinkedList implements Iterable<T>, so it can Iterate through elements
+        for (item in list) {
+            println("Double: ${item * 2}")
+        }
+        println()
+        // ---Example of printing doubles---
+        //Double: 4
+        //Double: -4
+        //Double: -6
+        //Double: 6
+        //Double: 8
+    }
+    
+    // *********** Becoming a collection **************
+    "removing elements".run {
+        val mutableList: MutableCollection<Int> = Mode.LinkedList()
+        mutableList.add(3)
+        mutableList.add(2)
+        mutableList.add(1)
+        
+        println("Before removing elements: $mutableList")
+        mutableList.remove(1)
+        println("After removing elements: $mutableList")
+        println()
+    }
+    
+    "retaining elements".run {
+        val mutableList: MutableCollection<Int> = Mode.LinkedList()
+        mutableList.add(3)
+        mutableList.add(2)
+        mutableList.add(1)
+        mutableList.add(4)
+        mutableList.add(5)
+    
+        println("Before retaining elements: $mutableList")
+        mutableList.retainAll(listOf(3, 4, 5))
+        println("After retaining elements: $mutableList")
+        println()
+    }
+    
+    "remove all elements".run {
+        val mutableList: MutableCollection<Int> = Mode.LinkedList()
+        mutableList.add(3)
+        mutableList.add(2)
+        mutableList.add(1)
+        mutableList.add(4)
+        mutableList.add(5)
+    
+        println("Before removing all elements: $mutableList")
+        mutableList.removeAll(listOf(3, 4, 5))
+        println("After removing all elements: $mutableList")
+        println()
+    }
+    
 }
